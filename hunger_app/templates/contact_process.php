a69035ae3fd624f022189bcbdbb43a92a885c1c7
himanshu991139 <himanshu991139@gmail.com>
2019-09-01 21:37:16 +0530

    'nav-bar-changes'

--- hunger_app/templates/contact_process.php
+++ hunger_app/templates/contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "himanshu991139@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
