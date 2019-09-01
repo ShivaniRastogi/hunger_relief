68907119caf49f112b90bf39f85a17e53bb930e2
yashi <yashiagnihotri22@gmail.com>
2019-09-02 00:20:27 +0530

    form and restaurant list api done

---
+++ hunger_app/view/restaurant.py
@@ -0,0 +1,42 @@
+
+from hunger_app.model.restaurant import Restaurant
+from hunger_app import app, db, cors
+from flask import jsonify, request
+from hunger_app.schema.restaurant import RestaurantSchema
+import datetime
+import time
+from itertools import cycle
+import json
+import decimal
+import flask.json
+
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+
+@app.route('/restaurant/form ', methods=['GET', 'POST'])
+def restaurant_form():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        post = Restaurant(**request.form.to_dict())
+        post.save()
+        result = RestaurantSchema().dump(post)
+        return jsonify({'result': {'restaurant': result.data}, 'message': "Success", 'error': False})
+
+
+@app.route('/restaurant/list', methods=['GET'])
+def restaurant_list():
+    if request.method == 'GET':
+        args = request.form.to_dict()
+        args.pop('page', None)
+        args.pop('per_page', None)
+        page = int(request.args.get('page', 1))
+        per_page = int(request.args.get('per_page', 10))
+        data = Restaurant.query.filter_by(**args).offset((page - 1) * per_page).limit(per_page).all()
+        result = RestaurantSchema(many=True).dump(data)
+        return render_template("hotel/payment/form.html", data=result.data)
+
